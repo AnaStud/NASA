748c5b4a38a357c8fdb0035477e03cdd56eb82d9
Anna <anasoft@yandex.ru>
2022-03-27 12:37:00 +0500

    Homework 6. Рефакторинг

--- app/src/main/java/ru/anasoft/nasa/view/recycler/RecyclerActivityAdapter.kt
+++ app/src/main/java/ru/anasoft/nasa/view/recycler/RecyclerActivityAdapter.kt
@@ -4,38 +4,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 import ru.anasoft.nasa.databinding.ActivityRecyclerItemEventBinding
 import ru.anasoft.nasa.databinding.ActivityRecyclerItemHeaderBinding
 import ru.anasoft.nasa.databinding.ActivityRecyclerItemNoteBinding
 
-class RecyclerActivityAdapter(val onClickItemListener:OnClickItemListener):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecyclerActivityAdapter(val onClickItemListener:OnClickItemListener):RecyclerView.Adapter<RecyclerActivityAdapter.BaseViewHolder>() {
 
-    lateinit var listData: List<Data>
+    private lateinit var listData: List<Data>
 
     fun setData(listData:List<Data>){
         this.listData = listData
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+
         return when(viewType){
             TYPE_EVENT -> {
-                val binding = ActivityRecyclerItemEventBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-                EventViewHolder(binding.root)
+                EventViewHolder(ActivityRecyclerItemEventBinding.inflate(LayoutInflater.from(parent.context),parent,false).root)
             }
             TYPE_HEADER-> {
-                val binding = ActivityRecyclerItemHeaderBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-                HeaderViewHolder(binding.root)
+                HeaderViewHolder(ActivityRecyclerItemHeaderBinding.inflate(LayoutInflater.from(parent.context),parent,false).root)
             }
             else-> {
-                val binding = ActivityRecyclerItemNoteBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-                NoteViewHolder(binding.root)
+                NoteViewHolder(ActivityRecyclerItemNoteBinding.inflate(LayoutInflater.from(parent.context),parent,false).root)
             }
         }
     }
 
-    inner class EventViewHolder(view:View):RecyclerView.ViewHolder(view){
-        fun bind(data: Data){
+    abstract class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        abstract fun bind(data: Data)
+    }
+
+
+    inner class EventViewHolder(view:View):BaseViewHolder(view){
+        override fun bind(data: Data){
             ActivityRecyclerItemEventBinding.bind(itemView).apply {
                 textViewName.text = data.name
                 textViewDescription.text = data.description
@@ -46,8 +48,8 @@ class RecyclerActivityAdapter(val onClickItemListener:OnClickItemListener):Recyc
         }
     }
 
-    inner class NoteViewHolder(view:View):RecyclerView.ViewHolder(view){
-        fun bind(data: Data){
+    inner class NoteViewHolder(view:View):BaseViewHolder(view){
+        override fun bind(data: Data){
             ActivityRecyclerItemNoteBinding.bind(itemView).apply {
                 textViewName.text = data.name
                 textViewDescription.text = data.description
@@ -58,8 +60,8 @@ class RecyclerActivityAdapter(val onClickItemListener:OnClickItemListener):Recyc
         }
     }
 
-    inner class HeaderViewHolder(view:View):RecyclerView.ViewHolder(view){
-        fun bind(data: Data){
+    inner class HeaderViewHolder(view:View):BaseViewHolder(view){
+        override fun bind(data: Data){
             ActivityRecyclerItemHeaderBinding.bind(itemView).apply {
                 tvName.text = data.name
                 itemView.setOnClickListener {
@@ -69,19 +71,8 @@ class RecyclerActivityAdapter(val onClickItemListener:OnClickItemListener):Recyc
         }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
-        when(getItemViewType(position)){
-            TYPE_EVENT -> {
-                (holder as EventViewHolder).bind(listData[position])
-            }
-            TYPE_HEADER -> {
-                (holder as HeaderViewHolder).bind(listData[position])
-            }
-            else-> {
-                (holder as NoteViewHolder).bind(listData[position])
-            }
-        }
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        holder.bind(listData[position])
     }
 
     override fun getItemViewType(position: Int): Int {
